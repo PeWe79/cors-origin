5ac9d0f8389831d2faf534247b160d0f15a98c3a
PeWe79 <poer.one79@gmail.com>
2023-11-02 12:32:37 +0700

    first commit.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var https = require('https');
-var unirest = require('unirest');
-var random_useragent = require('random-useragent');
-const isUrl = require("is-valid-http-url");
-const fs = require("fs");
+import { createServer } from 'https';
+import unirest from 'unirest';
+import { getRandom } from 'random-useragent';
+import isUrl from "is-valid-http-url";
+import { readFileSync } from "fs";
 
 var httpsOptions = {
-  key: fs.readFileSync("server.key"),
-  cert: fs.readFileSync("server.cert"),
+  key: readFileSync("server.key"),
+  cert: readFileSync("server.cert"),
 };
 
-https.createServer(httpsOptions, function (req, res) {
+createServer(httpsOptions, function (req, res) {
     res.writeHead(200, {
         "Access-Control-Max-Age":3600,
         "Access-Control-Allow-Origin": "*",
@@ -19,7 +19,7 @@ https.createServer(httpsOptions, function (req, res) {
     if (req.url.split("/?url=")[1] == undefined == false && isUrl(req.url.split("/?url=")[1]) == true && req.method === "GET") {
         unirest('GET', req.url.split("/?url=")[1])
             .headers({
-                'user-agent': random_useragent.getRandom()
+                'user-agent': getRandom()
             })
             .end(function (resku) {
                 res.end(resku.raw_body);
